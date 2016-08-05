c957c35d8ea98da6774698766e1a34157f08545b
clyde30 <ajonestx@gmail.com>
2016-08-04 23:58:25 -0600

    chore: add shuffle function

--- cardDealerApp/src/Deck.java
+++ cardDealerApp/src/Deck.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class Deck {
-  List deck = new ArrayList();
+  List newDeck = new ArrayList();
+  List shuffledDeck = new ArrayList();
 
   public Deck() {
     String[] suitList = {"Hearts", "Clubs", "Spades", "Diamonds"};
@@ -9,9 +10,23 @@ public class Deck {
     for (String suit : suitList) {
       for(int i = 2; i <= 14; i++) {
         Card card = new Card(i, suit);
-        deck.add(card);
-        card.printCard();
+        newDeck.add(card);
       }
     }
+    shuffle();
   }
+
+  public void shuffle() {
+    System.out.println("Shuffling");
+    List<Card> deck = newDeck;
+    while (0 < deck.size()){
+      Random r = new Random();
+		  int i = r.nextInt(deck.size());
+
+      Card card = deck.get(i);
+      card.printCard();
+      shuffledDeck.add(card);
+      deck.remove(i);
+  }
+}
 }
--- cardDealerApp/src/RunApp.java
+++ cardDealerApp/src/RunApp.java
@@ -7,19 +7,10 @@ import java.util.*;
 public class RunApp {
 
     public static void main(String []args) {
-      Deck deckOne = new Deck();
+      Deck deck = new Deck();
 
+      List<Card> cardList = deck.shuffledDeck;
 
-      // Scanner reader = new Scanner(System.in);
-      // System.out.println("Enter the number of cards to deal");
-      // int n = reader.nextInt();
-      //
-      // int numCards = n;
-
-
-      // Card cardOne = new Card(3, "Clubs");
-
-      // cardOne.printCard();
 
     }
 }
